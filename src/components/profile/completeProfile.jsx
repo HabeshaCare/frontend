4a5e57aed8b261958d82cb26ce5a324f9a0226fd
YeabNigusse <nigusseyeabsira@gmail.com>
2024-03-16 14:01:23 +0300

    almost done with profile page

--- src/components/profile/completeProfile.jsx
+++ src/components/profile/completeProfile.jsx
@@ -1,22 +1,39 @@
+import { CircularProgress } from "@/components/ui/progress";
 
-import { CircularProgress } from '@/components/ui/progress';
+const CompleteProfile = ({ progress }) => {
+  return (
+    <>
+      <div className="flex items-center bg-[#A7C2C5] mx-6 my-4 h-20 rounded-3xl">
+        <div className="mx-4">
+          <CircularProgress value={progress} />
+        </div>
+        <div className="text-md font-semibold">
+          {progress === 100 ? "Profile Completed!!" : " Complete Your Profile"}
+        </div>
+      </div>
+    </>
+  );
+};
 
+const CompleteProfile2 = ({ progress }) => {
+  return (
+    <>
+      <div className="flex flex-col items-center mt-8 bg-[#A7C2C5] h-72 w-64 rounded-3xl">
+        <div className="text-md font-semibold pt-4">
+          {progress === 100 ? "Profile Completed!!" : " Complete Your Profile"}
+        </div>
+        <div className="pt-4">
+          <CircularProgress value={progress} />
+        </div>
 
-const CompleteProfile = ({progress}) => {
+        <div className="mt-6 text-sm font-semibold">
+          <div className="mt-2">Profile Picture</div>
+          <div className="mt-2">General Info</div>
+          <div className="mt-2">Specific Info</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-    return(
-        <>
-         <div className='flex items-center bg-[#A7C2C5] mx-6 my-4 h-20 rounded-3xl'>
-                <div className='mx-4'>
-                <CircularProgress value={progress} />
-                </div>
-                <div className='text-md font-semibold'>
-                   {progress === 100 ? "Profile Completed!!" : " Complete Your Profile"}
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default CompleteProfile
+export { CompleteProfile, CompleteProfile2 };
