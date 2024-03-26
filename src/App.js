5d00883db912d03ccb655456a1532207e6011b53
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-03-26 22:40:17 +0300

    get user data file

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import Register from "./pages/auth/signup";
 import Login from './pages/auth/login';
-import Profile from '@/pages/patient/profile';
+import Profile from "./pages/patient/profile/Profile";
 import Dashboard from '@/pages/patient/dashboard';
 import VarifyEmail from "./components/auth/varifyEmail";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 
 const queryClient = new QueryClient()
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/verifyEmail"element={<VarifyEmail />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Register />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/verifyEmail" element={<VarifyEmail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Register />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
