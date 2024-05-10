9b222c25fa083667cd8d22c24c90c118d971d9e7
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-05-10 23:12:25 +0300

    LabForms

--- src/pages/laboratory/dashboard/requestForm.jsx
+++ src/pages/laboratory/dashboard/requestForm.jsx
@@ -3,13 +3,16 @@ import { Checkbox } from "@/components/ui/checkbox";
 import RequesterDetailsForm from "./Requesterdetails";
 import SampleDetailsForm from "./Sample details";
 import RelevantClinicalInformationForm from "./Relevant clinical information";
+import ExaminationRequestedForm from "./ExaminationRequestedForm";
 
 const ReqForm = () => {
     return (
         <div>
-            {/* <RequesterDetailsForm />
-            <SampleDetailsForm/> */}
-            {/* <RelevantClinicalInformationForm/> */}
+            <RequesterDetailsForm />
+            <SampleDetailsForm />
+            <RelevantClinicalInformationForm />
+            <ExaminationRequestedForm />
+
         </div>
     );
 };
