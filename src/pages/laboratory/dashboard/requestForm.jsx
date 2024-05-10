4d38ee7c728597ece61f72420f81535318014a7e
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-05-10 22:38:19 +0300

    RequesterDetailsForm

--- src/pages/laboratory/dashboard/requestForm.jsx
+++ src/pages/laboratory/dashboard/requestForm.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Checkbox } from "@/components/ui/checkbox";
 import RequesterDetailsForm from "./Requesterdetails";
+import SampleDetailsForm from "./Sample details";
+import RelevantClinicalInformationForm from "./Relevant clinical information";
 
 const ReqForm = () => {
     return (
         <div>
-            <RequesterDetailsForm />
+            {/* <RequesterDetailsForm />
+            <SampleDetailsForm/> */}
+            {/* <RelevantClinicalInformationForm/> */}
         </div>
     );
 };
