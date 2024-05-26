1059ed61b6d4411599b047b3a16312cd56bd8eaa
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-05-26 22:36:24 +0300

    profile fix

--- src/pages/laboratory/profile/index.jsx
+++ src/pages/laboratory/profile/index.jsx
@@ -30,7 +30,7 @@ const LaboratoryProfile = () => {
   return (
     <div className="p-4">
       <div className="md:flex">
-        <div className="md:w-3/4 mb-8">
+        <div className="md:w-2/3 mb-8">
           <div className="flex items-center my-4">
             <Link to="/laboratory/dashboard" className="ml-2 mt-2 cursor-pointer">
               <img src={back} alt="back icon" className="h-6 w-6" />
@@ -42,13 +42,13 @@ const LaboratoryProfile = () => {
 
           <div className="flex flex-col border border-solid mt-4 md:ml-24 p-14 rounded-lg shadow-md">
             <Picture image={laboratory} />
-            {isMdScreen ? "" : <CompleteProfile progress={80} />}
+            <CompleteProfile progress={80} />
 
-            <div className="flex justify-end mr-8 mt-4 gap-2">
+            <div className="flex justify-end mr-8 mt-4 gap-2" onClick={() => setEditMode(!editMode)}>
               <div>
-                <img src={edit} alt="edit SVG" className="h-6 w-6" />
+                <img src={edit} alt="edit SVG" className="h-4 w-4 mt-1" />
               </div>
-              <div className="text-[#1F555D] cursor-pointer" onClick={() => setEditMode(!editMode)}>
+              <div className="text-[#1F555D] cursor-pointer">
                 Edit
               </div>
             </div>
@@ -87,7 +87,7 @@ const LaboratoryProfile = () => {
                 {editMode ? <ProfileValue value="Verified" /> : <Input />}
               </div>
             </div>
-            
+
             <div className="text-xl text-[#1F555D] font-semibold font-serif mb-4 pl-8 mt-6">
               Admin Info
             </div>
@@ -120,57 +120,63 @@ const LaboratoryProfile = () => {
                 </Button>
               </div>
             )}
-          </div>
-        </div>
 
-        <div className="md:w-1/4 flex justify-center">
-          <div>
-            {isMdScreen && <CompleteProfile2 progress={80} />}
-            <div className="flex justify-center my-6">
-              {!editMode ? (
-                <label htmlFor="licenseUpload" className="cursor-pointer">
-                  <div className="w-64 h-20 flex flex-col font-semibold justify-center items-center border border-solid bg-gray-300 p-4 rounded-lg shadow-md">
-                    <FiUpload className="mr-2" />
-                    {licenseFile ? (
-                      <span>Reupload License</span>
-                    ) : (
-                      <span>Upload License</span>
-                    )}
-                    <input
-                      id="licenseUpload"
-                      type="file"
-                      accept=".pdf, .doc, .docx"
-                      style={{ display: "none" }}
-                      onChange={handleFileUpload}
-                    />
-                  </div>
-                </label>
-              ) : (
-                <div className="flex flex-col gap-2 items-center bg-gray-300 p-4 rounded-lg shadow-md">
-                  <div className="w-64 h-20 flex flex-col font-semibold justify-center items-center border border-solid gap-1">
-                    Uploaded License: <FiUpload className="mr-2" />
-                    {licenseFile ? (
-                      <span className="ml-8">{licenseFile.name}</span>
-                    ) : (
-                      <span className="text-gray-500">No file uploaded</span>
-                    )}
-                  </div>
-                  {licenseFile && (
-                    <button
-                      className="ml-4 text-red-600 hover:text-red-700 font-semibold"
-                      onClick={handleRemoveUpload}
-                    >
-                      Remove
-                    </button>
+
+
+
+            <div className=" flex justify-center">
+              <div>
+
+                <div className="flex justify-center my-6">
+                  {!editMode ? (
+                    <label htmlFor="licenseUpload" className="cursor-pointer">
+                      <div className="w-64 h-20 flex flex-col font-semibold justify-center items-center border border-solid bg-gray-300 p-4 rounded-lg shadow-md">
+                        <FiUpload className="mr-2" />
+                        {licenseFile ? (
+                          <span>Reupload License</span>
+                        ) : (
+                          <span>Upload License</span>
+                        )}
+                        <input
+                          id="licenseUpload"
+                          type="file"
+                          accept=".pdf, .doc, .docx"
+                          style={{ display: "none" }}
+                          onChange={handleFileUpload}
+                        />
+                      </div>
+                    </label>
+                  ) : (
+                    <div className="flex flex-col gap-2 items-center bg-gray-300 p-4 rounded-lg shadow-md">
+                      <div className="w-64 h-20 flex flex-col font-semibold justify-center items-center border border-solid gap-1">
+                        Uploaded License: <FiUpload className="mr-2" />
+                        {licenseFile ? (
+                          <span className="ml-8">{licenseFile.name}</span>
+                        ) : (
+                          <span className="text-gray-500">No file uploaded</span>
+                        )}
+                      </div>
+                      {licenseFile && (
+                        <button
+                          className="ml-4 text-red-600 hover:text-red-700 font-semibold"
+                          onClick={handleRemoveUpload}
+                        >
+                          Remove
+                        </button>
+                      )}
+                    </div>
                   )}
                 </div>
-              )}
+              </div>
             </div>
           </div>
+
         </div>
+
+
       </div>
     </div>
-      
+
   );
 };
 export default LaboratoryProfile;
