5e0d3d33c9269c39117352d28fa2227cdb2754bc
Yab <yabuman46@gmail.com>
2024-06-10 20:11:19 +0300

    refactor: Naming of the buttons

--- src/pages/video-call/FloatButtons.jsx
+++ src/pages/video-call/FloatButtons.jsx
@@ -4,9 +4,9 @@ import { Button } from '@/components/ui/button';
 function FloatButtons({isVideoEnabled, isAudioEnabled, toggleVideo, toggleAudio, leaveCall}) {
     return (
         <div className="fixed bottom-4 z-10 space-x-5" >
-           <Button variant="round" className="bg-gray-500 text-white" onClick={toggleAudio} >{isAudioEnabled ? 'NM' : 'M'}</Button> 
-           <Button variant="round" className="bg-blue-500 text-white" onClick={toggleVideo}>{isVideoEnabled ? 'NV': 'V'}</Button> 
+           <Button variant="round" className="bg-gray-500 text-white" onClick={toggleAudio} >{isAudioEnabled ? 'M' : 'NM'}</Button> 
            <Button variant="round" className="bg-red-500 text-white" onClick={leaveCall}>E</Button> 
+           <Button variant="round" className="bg-blue-500 text-white" onClick={toggleVideo}>{isVideoEnabled ? 'V': 'NV'}</Button> 
     
         </div>
     )
