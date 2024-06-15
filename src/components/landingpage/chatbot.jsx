6549c3e015850b2d809f4e79096ee6cc00e0b004
Yab <yabuman46@gmail.com>
2024-06-15 12:49:32 +0300

    feat: Added chat bot integration

--- src/components/landingpage/chatbot.jsx
+++ src/components/landingpage/chatbot.jsx
@@ -10,28 +10,55 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useToast } from "@/components/ui/use-toast";
 
-function Chatbot() {
+
+function Chatbot({context}) {
   const [input, setInput] = useState("");
   const [chatLog, setChatLog] = useState([
     { sender: "bot", message: "Hello, how can I help you?" },
   ]);
+  const token = useSelector((state) => state.auth.token);
+  const id = useSelector((state) => state.auth.user.id);
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (input.trim() === "") return;
 
     const newChatLog = [...chatLog, { sender: "user", message: input }];
     setChatLog(newChatLog);
     setInput("");
 
-    // Simulate LLM response
-    setTimeout(() => {
-      const response = {
-        sender: "bot",
-        message: "This is a simulated response from the LLM.",
-      };
-      setChatLog([...newChatLog, response]);
-    }, 1000);
+    const data = { query: input, patientHistory: context ? context : [] };
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      }
+    };
+    setIsLoading(true);
+    console.log(data);
+    try {
+      const response = await axios.post(
+        `http://localhost:5072/api/ai/conversation/${id}/chat`,
+        data,
+        config
+      );
+      const aiResponse = { sender: "bot", message: response.data.data.content };
+      setChatLog([...newChatLog, aiResponse]);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error! Error Occurred",
+        description: error.message ? error.message : "Something Went wrong"
+      });
+      setIsLoading(false);
+    }
+    setIsLoading(false);
   };
 
   const handleKeyDown = (event) => {
@@ -90,13 +117,13 @@ function Chatbot() {
               {chatLog.map((entry, index) => (
                 <div
                   key={index}
-                  className={`p-2 rounded-md ${
-                    entry.sender === "bot" ? "bg-white" : "bg-blue-100"
-                  }`}
+                  className={`p-2 rounded-md ${entry.sender === "bot" ? "bg-white" : "bg-blue-100"
+                    }`}
                 >
                   {entry.message}
                 </div>
               ))}
+              {isLoading && <p>Generating response...</p>}
             </SheetDescription>
           </SheetHeader>
           <SheetFooter>
