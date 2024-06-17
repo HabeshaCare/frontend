838c1daaf5f4f4960b29780fd0c41166d2ca2a44
YeabNigusse <nigusseyeabsira@gmail.com>
2024-06-17 19:51:57 +0300

    done with posting medical record

---
+++ src/lib/service/sendmedicalrecord.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const sendMedicalRecord = async (token, data) => {
+    console.log("data", data);
+    try {
+        const res = await axios.post(
+            "http://localhost:5072/api/records",
+            data,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+        return res.data;
+    } catch (error) {
+        throw error.response ? error.response.data : new Error("Network Error");
+    }
+};
+
+export default sendMedicalRecord;
