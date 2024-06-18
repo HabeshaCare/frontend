881cc4010ee2635bc3283c88d67e1f80f2a875dd
Yab <yabuman46@gmail.com>
2024-06-18 23:26:32 +0300

    fix: Root reducer conflict

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,10 +5,6 @@ import { patientReducer } from "./patientSlice";
 import adminReducer from "./adminSlice";
 import healthCenterReducer from "./healthcenterSlice";
 import scheduleReducer from "./scheduleSlice";
-import { combineReducers } from "@reduxjs/toolkit";
-import { authReducer } from "./authSlice";
-import { doctorReducer } from "./doctorSlice";
-import { patientReducer } from "./patientSlice";
 import { pharmacyReducer } from "./pharmacySlice";
 import { laboratoryReducer } from "./laboratorySlice";
 
