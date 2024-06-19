bf7db26e983d2d8aebb03446e098942d6b412093
YeabNigusse <nigusseyeabsira@gmail.com>
2024-06-19 08:57:36 +0300

    done with patient dashboard

--- src/pages/patient/dashboard/maincontent.jsx
+++ src/pages/patient/dashboard/maincontent.jsx
@@ -22,14 +22,14 @@ const MainContent = ({ activeLink }) => {
             {/* <div className="flex justify-center items-center m-2 p-4 text-white w-1/3 h-36 rounded-lg bg-gradient-to-b from-[#095FAF] to-[#02C27D]">
               Card 1
             </div> */}
-            <Card text="Card 1" />
-            <Card text="Card 2" />
-            <Card text="Card 3" />
+            <Card text="Look up a doctor according to your preferences." />
+            <Card text="Meet with your doctor through our online video call." />
+            <Card text="Access your medical data." />
           </div>
           <div className="flex justify-around w-full mt-8">
-            <Card text="Card 4" />
-            <Card text="Card 5" />
-            <Card text="Card 6" />
+            <Card text="Initiate a conversation with your personal assistant." />
+            <Card text="Complete your profile and increase your exposure." />
+            <Card text="Obtain your prescription from the pharmacy" />
           </div>
           <div className="flex justify-end">
             <Chatbot />
